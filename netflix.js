26eb7bb8507b029811a8ac9dfdaa5beea067669e
Avi Goldman <avrahamymgoldman@gmail.com>
2016-07-22 20:11:50 -0400

    set up player functions

--- netflix.js
+++ netflix.js
@@ -1,15 +1,115 @@
 function Netflix() {
-	var t = {
+
+
+	var netflix = {
 		// Constants
+		version: '0.1',
 		// Variables
-		// Setup
-		// Navigation
-			// Controls
-			// Information
+		
+		// browse
+		browse: {},
 		// Player
-			// Controls
-			// Information
+		player: {}
 	};
+
+
+	// Player Controls
+	netflix.play = function() {
+
+	};
+
+
+	netflix.pause = function() {
+
+	};
+
+
+	netflix.seek = function(time) {
+
+	};
+
+
+	netflix.currentTime = function() {
+
+	};
+
+
+	netflix.stabilize = function() {
+
+	};
+
+
+	netflix.getVolume = function() {
+
+	};
+
+
+	netflix.setVolume = function(volume) {
+
+	};
+
+
+	netflix.previousEpisode = function() {
+
+	};
+
+
+	netflix.nextEpisode = function() {
+
+	};
+
+
+	netflix.backToBrowse = function() {
+
+	};
+
+
+	// Player Information
+	netflix.getId = function() {
+
+	};
+
+
+	netflix.getTitle = function() {
+
+	};
+
+
+	netflix.getDescription = function() {
+
+	};
+
+
+	netflix.getType = function() {
+
+	};
+
+
+	netflix.getSeason = function() {
+
+	};
+
+
+	netflix.getEpisode = function() {
+
+	};
+
+
+	netflix.getEpisodeTitle = function() {
+
+	};
+
+
+	netflix.getEpisodeDescription = function() {
+
+	};
+
+
+	netflix.getLength = function() {
+
+	};
+
+
 
 
 	return t;
