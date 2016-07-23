f3735f1aa4d4a29475724a229dfe03505f24fd74
Avi Goldman <avrahamymgoldman@gmail.com>
2016-07-22 23:39:17 -0400

    added page functions

--- netflix.js
+++ netflix.js
@@ -1,116 +1,174 @@
-function Netflix() {
+function Netflix(jq) {
+
+	if (typeof jq !== 'undefined') {
+		$ = jq;
+	}
+	else if (typeof jQuery !== 'undefined') {
+		$ =jQuery;
+	}
+	else {
+		throw new Error("jQuery is required for netflix.js");
+	}
 
 
 	var netflix = {
 		// Constants
 		version: '0.1',
 		// Variables
-		
-		// browse
+		clickable: {},
+		// Pages
+		page: {},
+		// Browse
 		browse: {},
 		// Player
 		player: {}
 	};
 
+	// Location
+	var url = window.location.href;
+
+	netflix.page.isBrowse = function() {
+		return url.indexOf('/browse/') > 0;
+	};
+	
+	netflix.page.isPlayer = function() {
+		return url.indexOf('/watch/') > 0;
+	};
+
+	netflix.page.isTitle = function() {
+		return url.indexOf('/title/') > 0;
+	}
+
+	netflix.page.isSearch = function() {
+		return url.indexOf('/search/') > 0;
+	};
+
+	netflix.page.isSettings = function() {
+		var settingPages = ['ManageProfiles', 'YourAccount', 'email', 'password', 'phonenumber', 'YourAccountPayment', 'BillingActivity', 'ChangePlan', 'Subscribe', 'EmailPreferences', 'pin', 'DoNotTest', 'Activate', 'ManageDevices', 'EditProfiles', 'LanguagePreferences', 'HdToggle', 'SubtitlePreferences', 'MyListOrder', 'viewingactivity', 'MoviesYouveSeen', 'Reviews'];
+		for (var i = 0; i < settingPages.length; i++) {
+			if (url.indexOf('/'+settingPages[i]+'/') > 0)
+				return true;
+		}
+
+		return false;
+	};
+
+
+
 
 	// Player Controls
-	netflix.play = function() {
+	netflix.player.play = function() {
+		if (netflix.is)
+		if (netflix.player.isPaused())
+			netflix.clickable
+	};
+
+	netflix.player.pause = function() {
+
+	};
+
+	netflix.player.isPlaying = function() {
+
+	};
+
+	netflix.player.isPaused = function() {
+
+	};
+
+	netflix.player.seek = function(time) {
+
+	};
+
+	netflix.player.currentTime = function() {
+
+	};
+
+	netflix.player.stabilize = function() {
+
+	};
+
+	netflix.player.getVolume = function() {
+
+	};
+
+	netflix.player.setVolume = function(volume) {
+
+	};
+
+	netflix.player.previousEpisode = function() {
+
+	};
+
+	netflix.player.nextEpisode = function() {
+
+	};
+
+	netflix.player.backToBrowse = function() {
+
+	};
+
+	netflix.player.isFullscreen = function() {
+
+	};
+
+	netflix.player.toggleFullscreen = function() {
 
 	};
 
 
-	netflix.pause = function() {
 
-	};
-
-
-	netflix.seek = function(time) {
-
-	};
-
-
-	netflix.currentTime = function() {
-
-	};
-
-
-	netflix.stabilize = function() {
-
-	};
-
-
-	netflix.getVolume = function() {
-
-	};
-
-
-	netflix.setVolume = function(volume) {
-
-	};
-
-
-	netflix.previousEpisode = function() {
-
-	};
-
-
-	netflix.nextEpisode = function() {
-
-	};
-
-
-	netflix.backToBrowse = function() {
-
-	};
 
 
 	// Player Information
-	netflix.getId = function() {
+	netflix.player.getId = function() {
 
 	};
 
-
-	netflix.getTitle = function() {
-
-	};
-
-
-	netflix.getDescription = function() {
+	netflix.player.getTitle = function() {
 
 	};
 
-
-	netflix.getType = function() {
-
-	};
-
-
-	netflix.getSeason = function() {
+	netflix.player.getDescription = function() {
 
 	};
 
-
-	netflix.getEpisode = function() {
-
-	};
-
-
-	netflix.getEpisodeTitle = function() {
+	netflix.player.getType = function() {
 
 	};
 
-
-	netflix.getEpisodeDescription = function() {
+	netflix.player.getSeason = function() {
 
 	};
 
+	netflix.player.getEpisode = function() {
 
-	netflix.getLength = function() {
+	};
+
+	netflix.player.getEpisodeTitle = function() {
+
+	};
+
+	netflix.player.getEpisodeDescription = function() {
+
+	};
+
+	netflix.player.getLength = function() {
 
 	};
 
 
 
+
+
+	// find all elements
+	if (netflix.isPlayer()) {
+		netflix.clickable = {
+
+		};
+	}
+	else {
+
+	}
 
 	return t;
 }
